3cd8c8838b4b32437e198c45de412ef12f138d66
Kartikey Sharma <78343656+kartikey81@users.noreply.github.com>
2022-07-28 20:54:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import pickle
 app = Flask(__name__)
 model = pickle.load(open('houseprediction.pkl','rb')) 
 
-
 @app.route('/')
 def home():
   
@@ -22,7 +21,7 @@ def predict():
     For rendering results on HTML GUI
     '''
     home = float(request.args.get('home'))
-    sqft = float(request.args.get('SqFt'))
+    SqFt = float(request.args.get('SqFt'))
     bed = float(request.args.get('bed'))
     bath = float(request.args.get('bath'))
     offer = float(request.args.get('offers'))
@@ -30,10 +29,11 @@ def predict():
     neighborhood = float(request.args.get('neighborhood'))
     
 
-    prediction = model.predict([[home,sqft,bed,bath,offer,brick,neighborhood]])
+    prediction = model.predict([[home,SqFt,bed,bath,offer,brick,neighborhood]])
     
         
     return render_template('index.html', prediction_text='Regression Model  has predicted house price : {}'.format(prediction))
 
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
